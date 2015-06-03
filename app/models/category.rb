e48459ddc48fe6aba95ec9cd40f42f2c9ccdeb69
Bjornn Borg <bjornnborg@gmail.com>
2015-06-02 23:03:11 -0300

    creating full description in a path like structure for categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,8 @@
 class Category < ActiveRecord::Base
     belongs_to :user
+
+    def full_description
+        group = !self[:group].empty? ? "#{self[:group]}/" : "" 
+        "#{group}#{self[:description]}"
+    end
 end
