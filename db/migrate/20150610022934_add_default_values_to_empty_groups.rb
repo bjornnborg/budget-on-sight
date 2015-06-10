4083127a90e604b313c4dfcc1931699316f6a17d
Bjornn Borg <bjornnborg@gmail.com>
2015-06-10 01:26:06 -0300

    fixing query for update

--- db/migrate/20150610022934_add_default_values_to_empty_groups.rb
+++ db/migrate/20150610022934_add_default_values_to_empty_groups.rb
@@ -1,7 +1,7 @@
 class AddDefaultValuesToEmptyGroups < ActiveRecord::Migration
   def change
     say_with_time "Setting default value for empty groups..." do
-        Category.where(group: :nil).each do |category|
+        Category.where(group: "").each do |category|
             category.update_attribute :group, 'Default'
         end
     end
