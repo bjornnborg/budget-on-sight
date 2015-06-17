4acd50fa230c8083fdd2863c81ae3511c28452a5
Bjornn Borg <bjornnborg@gmail.com>
2015-06-16 23:40:07 -0300

    adding association extensions to improve dsl

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,19 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :categories, dependent: :destroy
-  has_many :transactions, dependent: :destroy
+  has_many :transactions, dependent: :destroy do 
+
+    def since(date)
+      where('date >= ?', date.strftime('%d/%m/%Y 00:00:00')) #at_beginning_of_day?
+    end
+
+    def until(date)
+      where('date <= ?', date.strftime('%d/%m/%Y 23:59:59')) #at_midnight?
+    end
+
+    def balance
+      all.sum(:amount)
+    end
+    
+  end
 end
