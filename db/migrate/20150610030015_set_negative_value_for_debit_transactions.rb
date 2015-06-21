52a5d7f92a907daad15559c79abbe9cefc9d6391
Bjornn Borg <bjornnborg@gmail.com>
2015-06-21 02:50:49 -0300

    refactoring sql criteria

--- db/migrate/20150610030015_set_negative_value_for_debit_transactions.rb
+++ db/migrate/20150610030015_set_negative_value_for_debit_transactions.rb
@@ -1,8 +1,8 @@
 class SetNegativeValueForDebitTransactions < ActiveRecord::Migration
   def change
     Transaction.joins(:category)
-      .where('amount > ?', 0)
-      .where('categories.category_type' => :debit).all.each do |transaction|
+      .where('amount > 0')
+      .where(category_type: :debit).find_each do |transaction|
         transaction.update_attributes :amount => transaction.amount * -1
     end
   end
