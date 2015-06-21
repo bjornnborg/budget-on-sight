c2e0a257f3335062c451482ba2022d6c0779a7be
Bjornn Borg <bjornnborg@gmail.com>
2015-06-21 03:53:31 -0300

    using transaction fixtures on specs

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,4 +1,12 @@
 FactoryGirl.define do
   factory :transaction do
+    date Time.zone.now
+    amount -100
+    association :category, strategy: :build
+  end
+
+  trait :credit do
+    amount 100
+    association :category, factory: [:category, :credit], strategy: :build
   end
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -3,34 +3,22 @@ require 'rails_helper'
 RSpec.describe Transaction, :type => :model do
   context "Normalize amount" do
     it "must change to negative amount if has a debit category" do
-      category = build(:category)
-      transaction = Transaction.new
-      transaction.category = category
-      transaction.amount = 100
+      transaction = build(:transaction, {amount: 100})
       expect(transaction.normalized_amount).to eq -100
     end
 
     it "must keep negative amount if has a debit category" do
-      category = build(:category)
-      transaction = Transaction.new
-      transaction.category = category
-      transaction.amount = -100
+      transaction = build(:transaction, {amount: -100})
       expect(transaction.normalized_amount).to eq -100
     end    
 
     it "must change to positive amount if has a credit category" do
-      category = build(:category, :credit)
-      transaction = Transaction.new
-      transaction.category = category
-      transaction.amount = -100
+      transaction = build(:transaction, :credit, {amount: -100})
       expect(transaction.normalized_amount).to eq 100
     end
 
     it "must keep positive amount if has a credit category" do
-      category = build(:category, :credit)
-      transaction = Transaction.new
-      transaction.category = category
-      transaction.amount = 100
+      transaction = build(:transaction, :credit, {amount: 100})
       expect(transaction.normalized_amount).to eq 100
     end
   end
