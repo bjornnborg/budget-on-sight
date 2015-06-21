9393b947cf4b471133893872452f46ba4a8c6476
Bjornn Borg <bjornnborg@gmail.com>
2015-06-21 02:56:04 -0300

    fixing tabs

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -3,36 +3,35 @@ require 'rails_helper'
 RSpec.describe Transaction, :type => :model do
   context "Negative and positive amounts" do
     it "must have negative amount if has a debit category" do
-        category = build(:category)
-        transaction = Transaction.new
-        transaction.category = category
-        transaction.amount = 100
-        expect(transaction.amount).to eq -100
+      category = build(:category)
+      transaction = Transaction.new
+      transaction.category = category
+      transaction.amount = 100
+      expect(transaction.amount).to eq -100
     end
 
     it "must keep negative amount if has a debit category" do
-        category = build(:category)
-        transaction = Transaction.new
-        transaction.category = category
-        transaction.amount = -100
-        expect(transaction.amount).to eq -100
+      category = build(:category)
+      transaction = Transaction.new
+      transaction.category = category
+      transaction.amount = -100
+      expect(transaction.amount).to eq -100
     end    
 
     it "must have positive amount if has a credit category" do
-        category = build(:category, :credit)
-        transaction = Transaction.new
-        transaction.category = category
-        transaction.amount = -100
-        expect(transaction.amount).to eq 100
+      category = build(:category, :credit)
+      transaction = Transaction.new
+      transaction.category = category
+      transaction.amount = -100
+      expect(transaction.amount).to eq 100
     end
 
     it "must keep positive amount if has a credit category" do
-        category = build(:category, :credit)
-        transaction = Transaction.new
-        transaction.category = category
-        transaction.amount = 100
-        expect(transaction.amount).to eq 100
+      category = build(:category, :credit)
+      transaction = Transaction.new
+      transaction.category = category
+      transaction.amount = 100
+      expect(transaction.amount).to eq 100
     end
   end
 end
-
