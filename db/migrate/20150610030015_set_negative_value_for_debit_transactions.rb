8ee9db70eb5848e32637c0720fa289d26c6aac43
Bjornn Borg <bjornnborg@gmail.com>
2015-06-21 02:46:01 -0300

    fixing indentation

--- db/migrate/20150610030015_set_negative_value_for_debit_transactions.rb
+++ db/migrate/20150610030015_set_negative_value_for_debit_transactions.rb
@@ -1,9 +1,9 @@
 class SetNegativeValueForDebitTransactions < ActiveRecord::Migration
   def change
     Transaction.joins(:category)
-        .where('amount > ?', 0)
-        .where('categories.category_type' => :debit).all.each do |transaction|
-            transaction.update_attributes :amount => transaction.amount * -1
+      .where('amount > ?', 0)
+      .where('categories.category_type' => :debit).all.each do |transaction|
+        transaction.update_attributes :amount => transaction.amount * -1
     end
   end
 end
