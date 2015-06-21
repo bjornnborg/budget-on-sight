0ccf33c02bbe21fb0473641cce8c46643bf94e9f
Bjornn Borg <bjornnborg@gmail.com>
2015-06-21 02:45:07 -0300

    Using find_each to take advantage of batch fetching for models

--- db/migrate/20150610022934_add_default_values_to_empty_groups.rb
+++ db/migrate/20150610022934_add_default_values_to_empty_groups.rb
@@ -1,9 +1,9 @@
 class AddDefaultValuesToEmptyGroups < ActiveRecord::Migration
   def change
     say_with_time "Setting default value for empty groups..." do
-        Category.where(group: "").each do |category|
-            category.update_attribute :group, 'Default'
-        end
+      Category.where(group: "").find_each do |category|
+        category.update_attribute :group, 'Default'
+      end
     end
   end
 end
