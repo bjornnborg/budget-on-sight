fb47a72c72d6fbee6ec190eeac4909eb4ce4e2cc
Bjornn Borg <bjornnborg@gmail.com>
2018-07-31 02:40:26 -0300

    Fix to consider frequency of transaction in order to compute hash. Wouldn't be able to find a monthly transaction if computing hash with some specific day

--- app/services/hash_service.rb
+++ app/services/hash_service.rb
@@ -3,7 +3,19 @@ require 'digest'
 class HashService
 
   def self.compute_missing_hash(transaction)
-    hash_input = "#{transaction.date}-#{transaction.category.full_description}-#{transaction.category.category_type}"
+    return if transaction.category.oftenly?
+
+    date_piece = transaction.date
+    if transaction.category.monthly?
+      date_piece = "#{transaction.date.at_beginning_of_month}-#{transaction.date.at_end_of_month}"
+    elsif transaction.category.weekly?
+      date_piece = "#{transaction.date.beginning_of_week(:sunday)}-#{transaction.date.end_of_week(:saturday)}"
+    elsif transaction.category.daily?
+      date_piece = "#{transaction.date}"
+    end
+    hash_input = "#{date_piece}-#{transaction.category.full_description}-#{transaction.category.category_type}"
+
+    puts "hash input: #{hash_input}"
     Digest::SHA256.hexdigest hash_input
   end
 
