9906138fe20ad22593886dc73286d85422771bd5
Bjornn Borg <bjornnborg@gmail.com>
2018-08-11 00:08:58 -0300

    Fixes and improvements on weekly suggestions

--- app/services/hash_service.rb
+++ app/services/hash_service.rb
@@ -11,8 +11,13 @@ class HashService
     elsif transaction.category.weekly?
       days_of_month = (transaction.date.beginning_of_month..Date.today).to_a
       dates = days_of_month.select{|d| d.wday == days_of_month.first.wday} # all days in the same week day as the start of month, until today
-      dates = dates.map{|d| [d, d + 6.days]}.first{|d| d >= transaction.date}.flatten # first start/end of week which contains the current date
+      #dates = dates.map{|d| [d, d + 6.days]}.first{|d| d.fisrt >= transaction.date}.flatten # first start/end of week which contains the current date
+      dates = dates.map{|d| [d, d + 6.days]}
+      puts "DATES TO HASH: #{dates}"
+      dates = dates.detect{|d| d.first >= transaction.date}.flatten # first start/end of week which contains the current date
       date_piece = "#{dates.first}-#{dates.last}"
+      puts "=====================date piece?"
+      puts "=====================#{date_piece}"
     elsif transaction.category.daily?
       date_piece = "#{transaction.date}"
     end
--- app/services/transaction_service.rb
+++ app/services/transaction_service.rb
@@ -15,6 +15,7 @@ class TransactionService
     result = {}
 
     [:daily, :weekly, :monthly].each do |frequency|
+    #[:weekly].each do |frequency|
       result[frequency] = get_missing_transactions(user, frequency)
     end
 
@@ -28,10 +29,12 @@ class TransactionService
     dates_to_iterate = dates
 
     if frequency == :monthly
-      dates_to_iterate = [Date.today]
+      dates_to_iterate = [[Date.today, Date.today]]
     elsif frequency == :weekly
-      dates_to_iterate = [dates.first]
+      #dates_to_iterate = [dates.first]
     end
+
+    dates_to_iterate = dates.map{|d| d.first}
 
     frequency_categories = Category.where(user_id: user.id, frequency: frequency).debits_first.to_a
     frequency_transactions_to_check = []
@@ -40,7 +43,10 @@ class TransactionService
         transaction = Transaction.new
         transaction.date = candidate_date
         transaction.category = c
-        transaction.missing_hash = HashService.compute_missing_hash(transaction)
+        puts ">>>>>>>>HASH PARA A DATA #{candidate_date}"
+        transaction.missing_hash = HashService.compute_missing_hash(transaction)        
+        puts ">>>>>>>>#{transaction.missing_hash}"
+        puts "------------------------"
         transaction
       end
     end
@@ -49,7 +55,7 @@ class TransactionService
 
     frequency_hashes = frequency_transactions_to_check.map{|t| t.missing_hash}
     
-    existing_transactions = Transaction.where(user_id: user.id, date: dates.first..dates.last)
+    existing_transactions = Transaction.where(user_id: user.id, date: dates.first.first..dates.last.last)
     existing_transactions_hashes = existing_transactions.map{|t| t.missing_hash}
 
     frequency_transactions_to_create = frequency_transactions_to_check.reject{|t| existing_transactions_hashes.include?(t.missing_hash)}
@@ -60,18 +66,25 @@ class TransactionService
     frequency_transactions_to_create
   end
 
+  #array of arrays with begin/end dates
   def self.get_dates_to_filter(frequency)
     today = Date.today
     dates = []
 
     if frequency == :daily
       dates = (Date.today.beginning_of_month..Date.today).to_a # all days until today
+      dates = dates.map{|e| [e, e]}
     elsif frequency == :weekly
       days_of_month = (today.beginning_of_month..today).to_a
       dates = days_of_month.select{|d| d.wday == days_of_month.first.wday} # all days in the same week day as the start of month, until today
-      dates = dates.map{|d| [d, d + 6.days]}.first{|d| d >= today}.flatten # first pair start/end of week which contains the current date
+      puts "Dates are >>>>>>>>>>>>>>>>>>>>>>>>>"
+      puts "#{dates}"      
+      #dates = dates.map{|d| [d, d + 6.days]}.first{|d| d >= today}.flatten # first pair start/end of week which contains the current date
+      dates = dates.map{|d| [d, d + 6.days]} # first pair start/end of week which contains the current date
+      puts "Dates pairs are >>>>>>>>>>>>>>>>>>>>>>>>>"
+      puts "#{dates}"
     else
-      dates = [today.beginning_of_month, today.end_of_month] #all days from the beginning up to the end of month
+      dates = [[today.beginning_of_month, today.end_of_month]] #all days from the beginning up to the end of month
     end
 
     dates
