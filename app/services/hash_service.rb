1b967922b3fe3fffe624323d9b1696158e8f68ca
Bjornn Borg <bjornnborg@gmail.com>
2018-08-11 00:33:56 -0300

    Using ranges to pick frequency date ranges

--- app/services/hash_service.rb
+++ app/services/hash_service.rb
@@ -12,9 +12,10 @@ class HashService
       days_of_month = (transaction.date.beginning_of_month..Date.today).to_a
       dates = days_of_month.select{|d| d.wday == days_of_month.first.wday} # all days in the same week day as the start of month, until today
       #dates = dates.map{|d| [d, d + 6.days]}.first{|d| d.fisrt >= transaction.date}.flatten # first start/end of week which contains the current date
-      dates = dates.map{|d| [d, d + 6.days]}
+      dates = dates.map{|d| (d..(d + 6.days))}
       puts "DATES TO HASH: #{dates}"
-      dates = dates.detect{|d| d.first >= transaction.date}.flatten # first start/end of week which contains the current date
+      #dates = dates.detect{|d| d.first >= transaction.date}.flatten # first start/end of week which contains the current date
+      dates = dates.detect{|d| d.cover?(transaction.date)}
       date_piece = "#{dates.first}-#{dates.last}"
       puts "=====================date piece?"
       puts "=====================#{date_piece}"
