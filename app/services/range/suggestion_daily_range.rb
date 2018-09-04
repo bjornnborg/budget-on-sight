cd0155f030df8245e344b619078688f54158c899
Bjornn Borg <bjornnborg@gmail.com>
2018-09-04 00:34:15 -0300

    Creating helper methods to get date intervals to build daily suggestions

--- app/services/range/suggestion_daily_range.rb
+++ app/services/range/suggestion_daily_range.rb
@@ -12,4 +12,16 @@ class Range::SuggestionDailyRange
     "#{self.date_piece}-category:#{@transaction.category.id}"
   end
 
+  def self.dates_to_filter(reference_date)
+    end_date = reference_date.end_of_month
+
+    on_this_month = Date.today.month == reference_date.month && Date.today.year == reference_date.year
+    if (on_this_month)
+      end_date = Date.today
+    end
+
+    dates = (reference_date.beginning_of_month..end_date).to_a # all days until today or the end of some month
+    dates = dates.map{|e| [e, e]}
+  end
+
 end
