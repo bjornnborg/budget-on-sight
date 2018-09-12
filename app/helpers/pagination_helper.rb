e2f7b86bb11c733d738d76086415fe7434111482
Bjornn Borg <bjornnborg@gmail.com>
2018-09-12 00:06:14 -0300

    Adding links to other features on each feature

--- app/helpers/pagination_helper.rb
+++ app/helpers/pagination_helper.rb
@@ -12,6 +12,11 @@ module PaginationHelper
     link_to 'Next month', "#{path}?year=#{next_month_date.strftime("%Y")}&month=#{next_month_date.strftime("%m")}"
   end
 
+  def get_month_link(label, path)
+    context_date = get_date
+    link_to label, "#{path}?year=#{context_date.strftime("%Y")}&month=#{context_date.strftime("%m")}"
+  end
+
   private
 
   def get_date
